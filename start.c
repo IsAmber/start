a4e3b3023b3e963575b04376c9c305e2314f821a
IsAmber <49730957+IsAmber@users.noreply.github.com>
2019-05-07 12:45:48 +0900

    Update start.c

--- start.c
+++ start.c
@@ -7,23 +7,19 @@ int main(int argc, char *argv[]) {
 	int count[100];
 	int countA[100];
 	int i, j, rank;
-
 	srand(time(NULL));
 	for(i=0; i<100; i++){
 		number[i] = rand() % 101;
 		count[i] = 0;
 	}
-	
 	for(i=0; i<100; i++){
 		for(j=0; j<100; j++){
 			if(number[i] == number[j]){
 				count[i] ++;
 			}
 		}
-	
 		count[i] -= 1;
 		printf("%d: %d\n", number[i], count[i]);
-		
 		for(i=0; i<100; i++)
 		{
 			rank = 1;
@@ -35,13 +31,11 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	}
-	
 	for(i=0; i<100; i++){
 		if(countA[i] == 1){
 			printf("1등은? %d번 %d번 나왔음\n", number[i], count[i]);
 			break;
 		}
 	}
-
 	return 0;
 }
